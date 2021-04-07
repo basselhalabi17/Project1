f0e38cd1d95722a28f5267b69159358225983c5a
chrisamgad@aucegypt.edu <chrisamgad@aucegypt.edu>
2021-04-07 22:12:33 +0200

    to Be fixed

--- Project1.c
+++ Project1.c
@@ -77,7 +77,25 @@ void ReRunMe(int delay, fptr f, int Priority){
         
         //insertintodelay(ready_queue.tasks[0].pointer_to_func, delay, ready_queue.tasks[0].priority);
         
-        //Add to delay queue
+        //decrement delays of other tasks that were already in the delay queue then insert the new task in the delay queue
+        for (int j=0;j<delay_queue.CURRENTSIZE;j++)//loop through delay queue to decrement the delay
+                {
+                    delay_queue.tasks[j].delay--; //decrement the delay of the task
+                    if (delay_queue.tasks[j].delay == 0) //if a delay of any task in the delay queue reached 0
+                    {
+                        QueTask(delay_queue.tasks[j].pointer_to_func,delay_queue.tasks[j].priority); //add the task to the ready queue                      
+                        //remove this task from delay queue
+                        for (int i=0;i<delay_queue.CURRENTSIZE;i++) //shift the tasks to move 1 index to the left(approching the head of the queue)
+                            delay_queue.tasks[i] = delay_queue.tasks[i+1];        
+                        //last task 3la el ymeen initialize b null
+                        delay_queue.CURRENTSIZE--;//decrement size of delay queue
+                        delay_queue.tasks[delay_queue.CURRENTSIZE].priority = -1;
+                        delay_queue.tasks[delay_queue.CURRENTSIZE].delay=99999;
+                        delay_queue.tasks[delay_queue.CURRENTSIZE].pointer_to_func = &Nothing;
+                        
+                    }
+            }
+        //Add the new task to delay queue
         delay_queue.tasks[delay_queue.CURRENTSIZE].delay=delay;
         delay_queue.tasks[delay_queue.CURRENTSIZE].priority= Priority;
         delay_queue.tasks[delay_queue.CURRENTSIZE].pointer_to_func =f;
@@ -99,49 +117,44 @@ void ReRunMe(int delay, fptr f, int Priority){
 
 void dispatch(){
     
-    bool NoTaskInReadyQueue=false;
-    if (ready_queue.CURRENTSIZE > 0){//if at this tick, there is a task that needs to get scheduled
+    bool enterReadyQueue=false;
+    //bool NoTaskInReadyQueue=false;
+    if(delay_queue.CURRENTSIZE>0)
+        if(delay_queue.tasks[0].delay==1)
+            enterReadyQueue=true;
+
+    if (ready_queue.CURRENTSIZE > 0) {//if at this tick, there is a task that needs to get scheduled
         printf("Enter dispatch and readqqueue!=0 ");
         ready_queue.tasks[0].pointer_to_func(); //call the task to execute  
+        //delay_queue.eltaskdi.delay --;
+
         for (int i=0;i<ready_queue.CURRENTSIZE;i++)
             ready_queue.tasks[i] = ready_queue.tasks[i+1];
-          
+        
         ready_queue.CURRENTSIZE--;
         ready_queue.tasks[ready_queue.CURRENTSIZE].priority = -1;
         ready_queue.tasks[ready_queue.CURRENTSIZE].delay=0;
-        ready_queue.tasks[ready_queue.CURRENTSIZE].pointer_to_func = &Nothing;
-        
-
+        ready_queue.tasks[ready_queue.CURRENTSIZE].pointer_to_func = &Nothing;      
     }
-    else if (ready_queue.CURRENTSIZE==0)//if no tasks in ready queue in this tick now
+    else if (ready_queue.CURRENTSIZE == 0 && enterReadyQueue==true) // if we have a task in the delay queue that has a delay of 1
+    {
+         ready_queue.tasks[0].priority=delay_queue.tasks[0].priority;
+         ready_queue.tasks[0].pointer_to_func=delay_queue.tasks[0].pointer_to_func;
+         ready_queue.tasks[0].delay=delay_queue.tasks[0].delay;   
+         ready_queue.CURRENTSIZE++; 
+
+         ready_queue.tasks[0].pointer_to_func(); //call the task to execute
+
+         ready_queue.CURRENTSIZE--;
+         ready_queue.tasks[ready_queue.CURRENTSIZE].priority = -1;
+         ready_queue.tasks[ready_queue.CURRENTSIZE].delay=0;
+         ready_queue.tasks[ready_queue.CURRENTSIZE].pointer_to_func = &Nothing;  
+    }
+
+    else if (ready_queue.CURRENTSIZE==0 && enterReadyQueue==false)//if no tasks in ready queue in this tick now
         {
             printf("No task scheduled lesa");
-
         }
-    
-    
-   if (delay_queue.CURRENTSIZE > 0)
-    {
-        for (int j=0;j<delay_queue.CURRENTSIZE;j++)//loop through delay queue to decrement the delay
-                {
-                    delay_queue.tasks[j].delay--; //decrement the delay of the task
-                    if (delay_queue.tasks[j].delay == 0) //if a delay of any task in the delay queue reached 0
-                    {
-                        QueTask(delay_queue.tasks[j].pointer_to_func,delay_queue.tasks[j].priority); //add the task to the ready queue
-                        
-                        //remove this task from delay queue
-                        for (int i=0;i<delay_queue.CURRENTSIZE;i++) //shift the tasks to move 1 index to the left(approching the head of the queue)
-                            delay_queue.tasks[i] = delay_queue.tasks[i+1];        
-                        //last task 3la el ymeen initialize b null
-                        delay_queue.CURRENTSIZE--;//decrement size of delay queue
-                        delay_queue.tasks[delay_queue.CURRENTSIZE].priority = -1;
-                        delay_queue.tasks[delay_queue.CURRENTSIZE].delay=99999;
-                        delay_queue.tasks[delay_queue.CURRENTSIZE].pointer_to_func = &Nothing;
-                        
-                    }
-                }
-    }
-    
     
     delayMs(50); //50ms after dispatch
     
@@ -149,10 +162,11 @@ void dispatch(){
 void print(struct taskQueue *q, int size){
     printf(" bassel alby");
     ReRunMe(1,&print,324);  //should get executed every tick
+    
 }
 void print2(struct taskQueue *q, int size){
     printf(" chris alby");
-    ReRunMe(0,&print2,568);// should get exectued every 3 ticks
+    ReRunMe(2,&print2,568);// should get exectued every 3 ticks
 }
 
 void print3(struct taskQueue *q, int size){
